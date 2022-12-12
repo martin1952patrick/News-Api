3ebf6f90ebee248ad338e63d28c8e4514f905527
martin1952patrick <martinpati1952@gmail.com>
2022-12-12 09:50:27 +0300

    commit main documents to the repo

---
+++ app/main/views.py
@@ -0,0 +1,22 @@
+# routes 
+from flask import render_template
+from . import main
+from ..request import get_news, get_sources, get_kenya_news
+
+
+@main.route("/")
+def index():
+    sources = get_sources()
+    news = get_news()
+    kenya = get_kenya_news()
+    return render_template("index.html", sources = sources, news = news, kenya = kenya)
+
+@main.route("/news")
+def news():
+    news = get_news()
+    kenya = get_kenya_news()
+    return render_template("news.html", news = news, kenya = kenya)
+
+@main.route("/about")
+def about():
+    return render_template("about.html")
